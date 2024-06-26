f2437eae59e8b15f89d7459dbea719fcfb8bf7f4
OmkarTeli <omkarteli8055@gmail.com>
2024-06-26 19:26:07 +0530

    refactor/ add router in delete function for refresh on delete task

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,14 +1,22 @@
 "use client";
 import React from "react";
 import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
 
 const DeleteButton = (props) => {
+   const router = useRouter();
    const handleDeleteTask = async () => {
       const confirmed = confirm("Do You Want To Delete Task..?");
       if (confirmed) {
-         await fetch(`http://localhost:3000/api/v1/todo?id=${props.id}`, {
-            method: "DELETE",
-         });
+         const res = await fetch(
+            `http://localhost:3000/api/v1/todo?id=${props.id}`,
+            {
+               method: "DELETE",
+            }
+         );
+         if (res.ok) {
+            router.refresh();
+         }
       }
    };
    return (
